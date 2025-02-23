357a249da723f611139b19a5b2157fe24c71a854
Fabs <Fabiolaayonperez97@gmail.com>
2025-02-23 15:13:12 +0100

    Configurado Webpack con soporte para JavaScript y Sass

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+  },
+  mode: 'development',
+};
